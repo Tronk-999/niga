cb821ddc14091146aa43a7229b22f2af9d840471
Tronk <trongnguyen123.123.123.123.4@gmail.com>
2024-08-21 16:36:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+document.getElementById('toggleButton').addEventListener('click', function() {
+        var textBox = document.getElementById('textBox');
+        var button = this;
+
+        if (textBox.classList.contains('hidden')) {
+            textBox.classList.remove('hidden');
+            textBox.classList.add('show');
+            button.classList.add('active');
+        } else {
+            textBox.classList.remove('show');
+            textBox.classList.add('hidden');
+            button.classList.remove('active');
+        }
+    });
